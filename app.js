b4ef184489ac98584a36f0f0632dab805c734cb0
Mohammed Kashif <65946187+kashifal@users.noreply.github.com>
2022-03-03 17:32:51 +0500

    regions

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-alert('hello Pakistan');
+
+
+function renderCountry(data) {
+  document.querySelector('.name h3').innerHTML = `${data.name.common}`;
+  document.querySelector('img').src = `${data.flags.png}`;
+  document.querySelector('.names h2').innerHTML = `${data.subregion}`;
+  document.querySelector('.capital h4').innerHTML = `${data.capital}`;
+  console.log(data);
+}
+
+
+
+const request = function (country) {
+  fetch(`https://restcountries.com/v3.1/capital/${country}`).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    renderCountry(data[0])
+  })
+}
+
+function inputted() {
+
+  const val = document.querySelector('input').value;
+
+  request(val)
+}
